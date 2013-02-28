09e9c6bcd82092c642837884434c309890828927
SCaddell@my.wctc.edu <>
2013-02-27 21:14:35 -0600

    Cash register, Receipt in progress, StartUp issue resolved

---
+++ src/sac/discountstrategy/Receipt.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sac.discountstrategy;
+
+/**
+ *
+ * @author Stuart - HP AMD 10
+ */
+public class Receipt {
+    
+    private String receiptItem;
+    private double totalDue;
+    private final static String RECEIPT_HEADING1
+            = "Thank you for shopping at Lombardo's\n";
+    //public Receipt(String customer, LineItem[] lineItems)
+    
+    public void createReceipt(Customer cust, LineItem[] lineItems) {
+        System.out.println(RECEIPT_HEADING1);
+        
+        for (int sub = 0; sub < lineItems.length; sub++ ) {
+            System.out.println(lineItems[sub].getQuanity() 
+                    + "  " + lineItems[sub].getItemId()
+                    + "  " + lineItems[sub].getItemDescription()
+                    + "\n"
+                    + "  " + lineItems[sub].getQuanity()
+                    + "  $" + lineItems[sub].getUnitPrice()
+                    + "  $" + lineItems[sub].getOriginalRetailPriceTotal()
+                    + "  $" + lineItems[sub].getDiscount()
+                    + "  $" + lineItems[sub].getNetPrice());
+                    
+        }
+    }
+            
+    
+}
---
+++ src/sac/discountstrategy/StartUp.java
@@ -0,0 +1,17 @@
+package sac.discountstrategy;
+
+/**
+ *
+ * @author Stuart Caddell
+ */
+public class StartUp {
+    
+    private CashRegister newRegisterTransaction = new CashRegister();
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    
+    //System.out.println(newRegisterTransaction.getScannedProductInfo("A101", 5));
+    
+    
+}
