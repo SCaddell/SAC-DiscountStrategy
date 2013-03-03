9afa7003441f59ad9a9da479363dd8a63178426c
SCaddell@my.wctc.edu <>
2013-03-02 21:19:30 -0600

    Saturday, initial format fix on receipt amounts

--- src/sac/discountstrategy/Receipt.java
+++ src/sac/discountstrategy/Receipt.java
@@ -23,14 +23,23 @@ public class Receipt {
         System.out.println(RECEIPT_HEADING1);
         DecimalFormat dollars = new DecimalFormat("#00.00");
         for (int sub = 0; sub < lineItems.length; sub++ ) {
+//            System.out.println(lineItems[sub].getItemId()
+//                    + "\t" + lineItems[sub].getItemDescription()
+//                    + "\n"
+//                    + lineItems[sub].getQuanity()
+//                    + "\t$" + dollars.format(lineItems[sub].getUnitPrice())
+//                    + "\t$" + dollars.format(lineItems[sub].getOriginalRetailPriceTotal())
+//                    + "\n\t\t$" + dollars.format(lineItems[sub].getDiscount())
+//                    + "\t$" + dollars.format(lineItems[sub].getNetPrice()));
             System.out.println(lineItems[sub].getItemId()
-                    + "\t" + lineItems[sub].getItemDescription()
-                    + "\n"
-                    + lineItems[sub].getQuanity()
-                    + "\t$" + dollars.format(lineItems[sub].getUnitPrice())
-                    + "\t$" + dollars.format(lineItems[sub].getOriginalRetailPriceTotal())
-                    + "\n\t\t$" + dollars.format(lineItems[sub].getDiscount())
-                    + "\t$" + dollars.format(lineItems[sub].getNetPrice()));
+                    + "\t" + lineItems[sub].getItemDescription());
+            System.out.print(lineItems[sub].getQuanity());
+            System.out.printf("\t%3.2f", lineItems[sub].getUnitPrice());
+            System.out.printf("\t%3.2f", 
+                    lineItems[sub].getOriginalRetailPriceTotal());
+            System.out.printf("\n    discount\t%3.2f", lineItems[sub].getDiscount());
+            System.out.printf("\t%3.2f", lineItems[sub].getNetPrice());
+            System.out.println("");
                     
         }
     }
